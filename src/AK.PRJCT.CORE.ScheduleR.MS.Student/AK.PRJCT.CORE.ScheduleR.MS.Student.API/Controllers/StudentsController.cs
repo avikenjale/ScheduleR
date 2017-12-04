dcb634a0ba5706c86571ea557e66cf93e8bc7c07
Avinash Kenjale <avikenjale@gmail.com>
2017-12-04 15:06:41 -0500

    Logging added to students service.

--- src/AK.PRJCT.CORE.ScheduleR.MS.Student/AK.PRJCT.CORE.ScheduleR.MS.Student.API/Controllers/StudentsController.cs
+++ src/AK.PRJCT.CORE.ScheduleR.MS.Student/AK.PRJCT.CORE.ScheduleR.MS.Student.API/Controllers/StudentsController.cs
@@ -4,43 +4,84 @@ using System.Linq;
 using System.Threading.Tasks;
 using AK.PRJCT.CORE.ScheduleR.MS.Student.Business.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace AK.PRJCT.CORE.ScheduleR.MS.Student.API.Controllers
 {
     public class StudentsController : Controller
     {
+        protected readonly ILoggerFactory LoggerFactory;
+        protected readonly ILogger Logger;
         protected readonly IStudentService StudentService;
 
-        public StudentsController(IStudentService studentService)
+        public StudentsController(IStudentService studentService, ILoggerFactory loggerFactory)
         {
             StudentService = studentService;
+
+            LoggerFactory = loggerFactory;
+            Logger = LoggerFactory.CreateLogger("LoggerCategory");
         }
 
         [HttpGet]
         [Route("")]
-        public IActionResult Warmpup(){
+        public IActionResult Warmpup()
+        {
             return Ok("Students service is running.");
         }
 
-        [HttpGet,  Route("api/[controller]")]
+        [HttpGet, Route("api/[controller]")]
         public async Task<IActionResult> GetStudentsAsync()
         {
             var students = await StudentService.GetStudentsAsync();
             return Ok(students);
         }
 
-        [HttpGet,  Route("api/[controller]/{name}")]
+        [HttpGet, Route("api/[controller]/{name}")]
         public async Task<IActionResult> GetStudentsByNameAsync(string name)
         {
-            var students = await StudentService.GetStudentsByNameAsync(name);
-            return Ok(students);
+            try
+            {
+                var students = await StudentService.GetStudentsByNameAsync(name);
+
+                if (students.Any())
+                {
+                    return Ok(students);
+                }
+                else
+                {
+                    Logger.LogInformation("No students found for given criteria.");
+                    return NotFound("No students found for given criteria.");
+                }
+            }
+            catch(Exception exception)
+            {
+                Logger.LogError(exception.Message);
+                return StatusCode(500);
+            }
         }
 
-        [HttpGet,  Route("api/[controller]/parent/{name}")]
+        [HttpGet, Route("api/[controller]/parent/{name}")]
         public async Task<IActionResult> GetStudentsByParentNameAsync(string name)
         {
-            var students = await StudentService.GetStudentsByParentNameAsync(name);
-            return Ok(students);
+            try
+            {
+                var students = await StudentService.GetStudentsByParentNameAsync(name);
+
+                if (students.Any())
+                {
+                    return Ok(students);
+                }
+                else
+                {
+                    Logger.LogInformation("No students found for given parent criteria.");
+                    return NotFound("No students found for given parent criteria.");
+                }
+            }
+            catch(Exception exception)
+            {
+                Logger.LogError(exception.Message);
+                return StatusCode(500);
+            }
         }
     }
 }
--- src/AK.PRJCT.CORE.ScheduleR.MS.Student/AK.PRJCT.CORE.ScheduleR.MS.Student.API/Startup.cs
+++ src/AK.PRJCT.CORE.ScheduleR.MS.Student/AK.PRJCT.CORE.ScheduleR.MS.Student.API/Startup.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace AK.PRJCT.CORE.ScheduleR.MS.Student.API
 {
@@ -19,15 +21,20 @@ namespace AK.PRJCT.CORE.ScheduleR.MS.Student.API
         {
             services.AddTransient(typeof(Data.Repositories.IStudentRepository), typeof(Data.Repositories.StudentRepository));
             services.AddTransient(typeof(Data.Services.IStudentDataService), typeof(Data.Services.StudentDataService));
-             services.AddTransient(typeof(Business.Services.IStudentService), typeof(Business.Services.StudentService));
-            services.AddMvc();            
-            
-            
+            services.AddTransient(typeof(Business.Services.IStudentService), typeof(Business.Services.StudentService));
+
+            services.AddSingleton(typeof(ILoggerFactory), typeof(LoggerFactory));  
+
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddDebug();
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
